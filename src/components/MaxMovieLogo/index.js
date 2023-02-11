27f4ad5fe1314ac9e5ad38e97e7972f183c99024
Davidson Ratis <davidsonmakarios@gmail.com>
2023-02-11 11:39:37 -0300

    created - project logo

--- src/components/MaxMovieLogo/index.js
+++ src/components/MaxMovieLogo/index.js
@@ -1,9 +1,11 @@
 import './style.css';
+import logoImg from '../../assets/img/movie.svg';
 
 const MaxMovieLogo = () => {
   return (
     <div className="max-movie-logo">
-      <h3>MaxMovie</h3>
+      <img className="max-movie-logo__img" src={logoImg} alt="logo" />
+      <h2 className="max-movie-logo__text">MaxMovie</h2>
     </div>
   )
 }
