2b39d80167a151f5b96d3be199eee29a672e39f0
Davidson Ratis <davidsonmakarios@gmail.com>
2023-02-11 11:36:39 -0300

    imported - header and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import AppRoutes from './routes';
+import AppHeader from './template/AppHeader';
 
 function App() {
   return (
     <div className="App">
+      <AppHeader />
       <AppRoutes />
     </div>
   );
