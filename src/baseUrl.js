b3073024be34d22ef79598ef6b9fd4c6bdb9d669
Davidson Ratis <davidsonmakarios@gmail.com>
2023-02-11 09:57:14 -0300

    created - baseUrl

---
+++ src/baseUrl.js
@@ -0,0 +1,6 @@
+export const apiUrl = "https://api.themoviedb.org/3";
+export const moviesPopular = "/discover/movie?sort_by=popularity.desc&";
+export const moviesSearch = "/search/movie/?";
+export const movieGenreList = "/genre/movie/list?";
+export const movieImg = "https://image.tmdb.org/t/p/w500/";
+export const movieLang = "&language=pt-br";
