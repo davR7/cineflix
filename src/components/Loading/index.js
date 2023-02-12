e57a2742a85df8af9e1dd94379d1465f17d7da7c
Davidson Ratis <davidsonmakarios@gmail.com>
2023-02-11 21:36:39 -0300

    updated - component Loading v1.1

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -2,7 +2,9 @@ import './style.css';
 
 const Loading = () => {
   return (
-    <div className="lds-dual-ring"></div>
+    <div className="loading">
+      <div className="lds-dual-ring"></div>
+    </div>
   )
 }
 
