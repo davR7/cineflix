dab86a440feb193911a920473cebbb76c80a5189
Davidson Ratis <davidsonmakarios@gmail.com>
2023-02-12 10:08:28 -0300

    updated - hook useHttp v2.0

--- src/hooks/useHttp.js
+++ src/hooks/useHttp.js
@@ -2,15 +2,25 @@ import { useState, useEffect } from "react";
 
 const useHttp = url => {
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     fetch(url)
-      .then(res => res.json())
-      .then(res => setData(res))
-      .catch(err => setData({ error: err.message }))
+      .then(res => {
+        if (!res.ok){
+          return Promise.reject(
+            `Ops! Algo deu errado. Verique sua Conexão ou Informe ao Admin do Sistema.`
+          );
+        } 
+        return res.json()
+      })
+      .then(setData)
+      .catch(setError)
+      .finally(setLoading(false))
   }, [url])
 
-  return [data]
+  return [data, error, loading]
 };
 
 export default useHttp;
