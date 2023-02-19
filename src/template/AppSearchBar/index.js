1dd73b9a12d927a5f1db62dae0b13319ca2e14cf
Davidson Ratis <davidsonmakarios@gmail.com>
2023-02-19 14:23:16 -0300

    updated - component AppSearchBar v1.5

--- src/template/AppSearchBar/index.js
+++ src/template/AppSearchBar/index.js
@@ -1,14 +1,27 @@
 import './style.css';
 import magnifier from '../../assets/img/magnifier.png'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const AppSearchBar = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handlerSubmit = e => {
+    e.preventDefault();
+    if (!search) return;
+    navigate(`/search?q=${search}`);
+    setSearch("");
+  }
 
   return (
-    <form className="app-search-bar">
+    <form className="app-search-bar" onSubmit={handlerSubmit}>
       <input 
         type="text" 
         placeholder="Pesquise aqui..." 
         className="app-search-bar__field"
+        onChange={({target}) => setSearch(target.value)}
+        value={search}
       />
       <button type="submit" className="app-search-bar__btn">
         <img className="app-search-bar__img" src={magnifier} alt="search bar" />
