854c24c997673613c4de9f94e8753f98a4ea23a5
Davidson Ratis <davidsonmakarios@gmail.com>
2023-02-19 14:13:29 -0300

    updated - component MovieCard v1.5

--- src/components/MovieCard/index.js
+++ src/components/MovieCard/index.js
@@ -1,16 +1,20 @@
 import './style.css';
 import { movieImg } from '../../baseUrl'
+import noImage from '../../assets/img/no_image.png'
 
 const MovieCard = ({movie, allGenres}) => {
   const totGenres = allGenres && allGenres.genres
     .filter(genre => movie.genre_ids.indexOf(genre.id) > -1);
   movie.genre_names = totGenres;
+  
+  const img = movie.poster_path ? movieImg + movie.poster_path : noImage;
 
   return (
     <div className="movie-card">
-      <img src={movieImg + movie.poster_path} alt={movie.title} />
+      <img className="movie-card__img" src={img} alt={movie.title} />
+      <p className="movie-card__title">Título: {movie.title}</p>
       <div className="genres">
-        <h3 className="genres__title">Gêneros:</h3>
+        <p className="genres__title">Gênero:</p>
         <ul className="genres__list">
           {movie.genre_names !== null && 
             movie.genre_names.map(genre => (
