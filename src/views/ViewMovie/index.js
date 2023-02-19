e1b5460c2e9f43494c986bb5f34d3802c694b94d
Davidson Ratis <davidsonmakarios@gmail.com>
2023-02-19 14:56:51 -0300

    updated - component ViewMovie v1.5

--- src/views/ViewMovie/index.js
+++ src/views/ViewMovie/index.js
@@ -1,10 +1,33 @@
 import './style.css';
+import { apiBaseUrl, movieLang } from '../../baseUrl';
+import useFetch from '../../hooks/useFetch';
+import { useParams } from 'react-router-dom';
+import MovieDetails from '../../components/MovieDetails';
+import MovieTrailer from '../../components/MovieTrailer';
 
 const ViewMovie = () => {
+  const { id } = useParams();
+  const { REACT_APP_API_KEY } = process.env
+
+  const {
+    data: movieDetails,
+    error: errorMovieDetails,
+    loading: loadingMovieDetails
+  } = useFetch(`${apiBaseUrl}/movie/${id}?${REACT_APP_API_KEY}&append_to_response=videos&${movieLang}`);
+
   return (
-    <div className="view-movie">
-      <h3>Movie</h3>
-    </div>
+    <main className="view-movie">
+      <MovieDetails
+        movieDetails={movieDetails}
+        loadingMovieDetails={loadingMovieDetails}
+        errorMovieDetails={errorMovieDetails}
+      />
+      <MovieTrailer
+        trailer={movieDetails
+          && movieDetails.videos}
+        trailerLoading={loadingMovieDetails}
+      />
+    </main>
   )
 }
 
