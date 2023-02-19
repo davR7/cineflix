2ca0ac23d2aece62cb2b85f7e2fc2b09aed08fbb
Davidson Ratis <davidsonmakarios@gmail.com>
2023-02-19 14:36:32 -0300

    updated - changed page title

--- src/views/ViewHome/index.js
+++ src/views/ViewHome/index.js
@@ -4,7 +4,7 @@ import './style.css';
 const ViewHome = () => {
   return (
     <div className="view-home">
-      <h3 className="page-title">Filmes Populares</h3>
+      <h3 className="page-title">Os Mais Populares</h3>
       <PopularMovies />
     </div>
   )
