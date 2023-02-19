4bb5cffc5cae08c576756c3c6a0582c58d7775c3
Davidson Ratis <davidsonmakarios@gmail.com>
2023-02-19 18:09:02 -0300

    created - function scrollToTopSmooth

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,3 +4,11 @@ export const formatDate = (data) => {
   let year  = data.split("-")[2];
   return year + '/' + ("0"+month).slice(-2) + '/' + ("0"+day).slice(-2);
 }
+
+export const scrollToTopSmooth = () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth', 
+  });
+}
