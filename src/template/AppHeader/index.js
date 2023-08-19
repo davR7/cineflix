b23abc8000cfc705cef600cf6d18760b66ea728c
Davidson Ratis <davidsonmakarios@gmail.com>
2023-08-19 19:00:00 -0300

    imported - component CineflixLogo

--- src/template/AppHeader/index.js
+++ src/template/AppHeader/index.js
@@ -1,17 +1,17 @@
-import './style.css';
-import MaxMoviesLogo from '../../components/MaxMoviesLogo';
-import AppSearchBar from '../AppSearchBar';
-import { Link } from 'react-router-dom';
+import "./style.css";
+import CineflixLogo from "../../components/CineflixLogo";
+import AppSearchBar from "../AppSearchBar";
+import { Link } from "react-router-dom";
 
 const AppHeader = () => {
   return (
     <div className="app-header">
       <Link to="/">
-        <MaxMoviesLogo />
+        <CineflixLogo />
       </Link>
       <AppSearchBar />
     </div>
-  )
-}
+  );
+};
 
 export default AppHeader;
