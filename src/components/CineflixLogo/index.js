9bd9caee3765af6ab111307e9ffbc4d44e34c34a
Davidson Ratis <davidsonmakarios@gmail.com>
2023-08-19 18:56:27 -0300

    modified - app logo

---
+++ src/components/CineflixLogo/index.js
@@ -0,0 +1,15 @@
+import "./style.css";
+import logoImg from "../../assets/img/logo.png";
+
+const CineflixLogo = () => {
+  return (
+    <div className="cineflix-logo">
+      <div className="cineflix-logo__wrapper">
+        <img className="cineflix-logo__img" src={logoImg} alt="logo" />
+        <h2 className="cineflix-logo__text">Cineflix</h2>
+      </div>
+    </div>
+  );
+};
+
+export default CineflixLogo;
