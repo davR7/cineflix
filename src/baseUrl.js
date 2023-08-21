ab3efb530f86f57f1eb67ea6c9795a402721ee38
Davidson Ratis <davidsonmakarios@gmail.com>
2023-08-20 21:31:54 -0300

    deleted file after modification

--- src/baseUrl.js
+++
@@ -1,6 +0,0 @@
-export const apiBaseUrl = "https://api.themoviedb.org/3";
-export const moviesPopular = "/discover/movie?sort_by=popularity.desc";
-export const moviesSearch = "/search/movie";
-export const movieGenreList = "/genre/movie/list";
-export const movieImg = "https://image.tmdb.org/t/p/w500/";
-export const movieLang = "language=pt-br";
